fa558954cf452efd0d895a36d06f510344ca3dc0
Marcel Gotardo <xmngotardo@outlook.com>
2023-01-31 18:08:52 -0300

    v1.0

---
+++ Scripts/temp.js
@@ -0,0 +1,7 @@
+// Info.
+let telNumber = '5522998862880';
+let emailAddress = 'xmngotardo@outlook.com';
+let linkedinAddress = 'https://www.linkedin.com/in/xmngotardo/';
+let githubAddress = 'https://www.github.com/xmngotardo/';
+
+let ano = Date().getFullYear();
