11b6e36c8415fe1361597b118028fd1d6aa35043
Marcel Gotardo <114816841+xgotardim@users.noreply.github.com>
2023-09-09 11:12:30 +0000

    Update temp.js

--- Scripts/temp.js
+++ Scripts/temp.js
@@ -1,7 +1,7 @@
 // Info.
 let telNumber = '5522998862880';
-let emailAddress = 'xmngotardo@outlook.com';
-let linkedinAddress = 'https://www.linkedin.com/in/xmngotardo/';
-let githubAddress = 'https://www.github.com/xmngotardo/';
+let emailAddress = 'xgotardim@outlook.com';
+let linkedinAddress = 'https://www.linkedin.com/in/xgotardim/';
+let githubAddress = 'https://www.github.com/xgotardim/';
 
-let ano = Date().getFullYear();
+// let ano = Date().getFullYear();
