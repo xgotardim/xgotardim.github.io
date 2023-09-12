db844689fe42e9fbdddffa352ce714460edc6e95
Marcel Gotardo <xgotardim@outlook.com>
2023-09-11 21:16:31 -0300

    Reparo link linkedin.

--- Scripts/temp.js
+++ Scripts/temp.js
@@ -1,7 +1,7 @@
 // Info.
 let telNumber = '5522998862880';
 let emailAddress = 'xgotardim@outlook.com';
-let linkedinAddress = 'https://www.linkedin.com/in/xgotardim/';
+let linkedinAddress = 'https://br.linkedin.com/in/xgotardim/';
 let githubAddress = 'https://www.github.com/xgotardim/';
 
 let currentYear = new Date().getFullYear();
