7397b6a5c936a3c79ce8e5fde3fa3ab80be47448
Marcel Gotardo <xgotardim@outlook.com>
2023-09-11 21:06:09 -0300

    Atualização.

--- Scripts/temp.js
+++ Scripts/temp.js
@@ -4,4 +4,6 @@ let emailAddress = 'xgotardim@outlook.com';
 let linkedinAddress = 'https://www.linkedin.com/in/xgotardim/';
 let githubAddress = 'https://www.github.com/xgotardim/';
 
-// let ano = Date().getFullYear();
+let currentYear = new Date().getFullYear();
+
+document.getElementById('copyright-text').innerHTML += currentYear;
